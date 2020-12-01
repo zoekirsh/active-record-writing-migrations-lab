e8dc05b298b34da331f52fe872d7f110ba8b2501
Zoe Kirsh <zoekirsh@gmail.com>
2020-11-30 18:25:10 -0800

    done

---
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -0,0 +1,12 @@
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+    def up 
+    end
+
+    def down
+    end
+
+    def change 
+        change_column :students, :birthdate, :datetime
+    end
+
+end
